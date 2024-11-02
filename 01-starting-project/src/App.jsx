78d7b3ca3f3c59099438de043161c4600d08d439
The-Void07 <summitchhetri20@gmail.com>
2024-11-02 11:34:48 +0530

    Up until now we have learnt the basics of react and how to maintain the files for more cleaner and readable codes.

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import { CoreConcept } from "./components/core-concepts.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {/* Down here is given two ways in which we can generate the same
+          working elements. */}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-starting-project/src/components/core-concepts.jsx
@@ -0,0 +1,11 @@
+// We can directly pass the object as arguments to a function
+//and use them as shown below.
+export function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
