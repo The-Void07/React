f7b37b8be6dc5d01491f5466ab622d4829ecd8ba
The-Void07 <summitchhetri20@gmail.com>
2024-11-25 20:15:53 +0530

    In this section, we have created the ux. Furthermore, we learnt about the ancistor component and its usecases.

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,34 @@
 import Player from "./Components/Player.jsx";
 import Gameboard from "./Components/Gameboard.jsx";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player-1" symbol="X" />
-          <Player initialName="Player-2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player-1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player-2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
       </div>
-      <Gameboard />
+      <Gameboard
+        onSelectSquare={handleSelectSquare}
+        activePlayerSymbol={activePlayer}
+      />
     </main>
   );
 }
